036cc325d925f849238d10bf8d85df6a6b59bf7b
aledezma25 <aledezma@unal.edu.co>
2022-05-06 17:37:24 -0500

    Se suben archivos

--- validar.js
+++ validar.js
@@ -1,3 +1,96 @@
+function habilitar(){
+    const radio = document.getElementById("flexRadioDefault1");
+    if (radio.checked == true)
+    {
+       let checkbox = document.getElementById("form-checkbox");       
+       checkbox.style.display = "block";
+    }
+    else
+    {
+        let checkbox = document.getElementById("form-checkbox");       
+        checkbox.style.display = "none";  
+    }
+}
+
+function upperCaseAlert(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+var nombre = document.getElementById("nombre");
+var p1 = document.getElementById("pass1").value;
+var p2 = document.getElementById("pass2").value;
+var error = document.getElementById("error");
+error.style.color = 'red';
+function enviarFormulario()
+{
+    console.log("Enviando Formulario...");
+        var mensajesError = [];
+        if(nombre.value === null || nombre.value === ''){
+            mensajesError.push("Ingresa tu nombre");
+        }
+
+        error.innerHTML = mensajesError.join(", ");
+    return false;
+}
+
+ INTENTOS FALLIDOS
+function verificar(){
+    let nombre = document.getElementById("nombre").value;
+    if (nombre == "" )
+    {
+        console.log("El nombre esta vacio");
+    }
+    else{
+        
+    }
+}
+
+
 /*function seleccion{
     let 
     let input1= document.getElementById("FlexRadioDefault1")
@@ -27,3 +120,4 @@ function validarpass{
     return false;
     }
 }
+*/
