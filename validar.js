598221ac018235539fe81eb4ae05d10dcb0bd61c
aledezma25 <aledezma@unal.edu.co>
2022-05-06 18:07:23 -0500

    Se agrega funcion en el input nombre

--- validar.js
+++ validar.js
@@ -13,111 +13,15 @@ function habilitar(){
 }
 
 function upperCaseAlert(){
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-var nombre = document.getElementById("nombre");
-var p1 = document.getElementById("pass1").value;
-var p2 = document.getElementById("pass2").value;
-var error = document.getElementById("error");
-error.style.color = 'red';
-function enviarFormulario()
-{
-    console.log("Enviando Formulario...");
-        var mensajesError = [];
-        if(nombre.value === null || nombre.value === ''){
-            mensajesError.push("Ingresa tu nombre");
-        }
-
-        error.innerHTML = mensajesError.join(", ");
-    return false;
-}
-
- INTENTOS FALLIDOS
-function verificar(){
-    let nombre = document.getElementById("nombre").value;
-    if (nombre == "" )
+    const nombre = document.getElementById("nombre");
+    if (nombre.value != "")
     {
-        console.log("El nombre esta vacio");
-    }
-    else{
-        
+        alert(`${nombre.value.toUpperCase()}`)
     }
 }
 
 
-/*function seleccion{
-    let 
-    let input1= document.getElementById("FlexRadioDefault1")
-    if(input1.checked)
+function emailEssay()
+{
+    document.getElementById("boton").disabled = true;
 }
-
-
-
-
-
-
-
-
-function validarpass{
-    var p1 = document.getElementById("pass1").value;
-    var p2 = document.getElementById("pass2").value;
-    var espacios = false;
-    var cont = 0;
-    while (!espacios && (cont < p1.length)) {
-        if (p1.charAt(cont) == " ")
-            espacios = true;
-            cont++;
-}
-   
-    if (espacios) {
-      alert ("La contraseña no puede contener espacios en blanco");
-    return false;
-    }
-}
-*/
